1411973f6964ab57fa13fad7e62a307235f26eee
virender-codevian <virender.codevian@gmail.com>
2021-07-12 23:56:53 +0530

    color update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,18 +3,53 @@ import "../../assets/scss/style.scss";
 import { Link } from "react-router-dom";
 import {skillList} from "../../assets/data/skillsData"
 import Skillcard from "../skills/Skillcard";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaBootstrap,
+  FaSass,
+  FaReact,
 
+  FaVuejs,
+  FaDocker,
+
+  FaNode,
+} from "react-icons/fa"
+import styled from "styled-components"
+import {   DiJava } from "react-icons/di";
+import { IoLogoJavascript } from "react-icons/io"
+import { GrGatsbyjs, GrMysql } from "react-icons/gr"
+import { SiTypescript, SiJest, SiPostman } from "react-icons/si"
 
 const Home = () => {
   return (
     <main className="home-container">
       <div class="HomePageLogo hXikou">
-        <div class="HomePageLogo dnzAPl  bXsdap">virender</div>
+        <div class="HomePageLogo dnzAPl  Effect">virender</div>
       </div>
    
       <p class="creative-developer">A Creative Developer</p>
 
-         
+      <Icons>
+        <FaHtml5 />
+        <FaCss3Alt />
+        <FaSass />
+        <FaBootstrap />
+        <IoLogoJavascript />
+        <FaReact />
+        <FaNode/>
+        <GrGatsbyjs />
+        {/* <FaVuejs /> */}
+        <SiTypescript />
+        {/* <SiJest /> */}
+        {/* <FaDocker /> */}
+        <DiJava />
+        <GrMysql />
+        <SiPostman />
+      </Icons>
+
+
+{/*          
          <div className="skillcontainer">
 
            {
@@ -22,7 +57,7 @@ const Home = () => {
                return <Skillcard skillName = {skillName} skillUrl = { skillUrl}/>
              })
            }
-         </div>
+         </div> */}
     
       <div class="home-page-btn">
         <Link to="/projects">
@@ -39,7 +74,20 @@ const Home = () => {
         </a>
       </div>
     </main>
+
+
   );
 };
 
+
+const Icons = styled.p`
+  font-size: 24px;
+  margin: 0;
+  margin-top: -0.5rem;
+  text-align: center;
+  @media (max-width: 768px) {
+    font-size: 22px;
+    margin-top: -16px;
+  }
+`
 export default Home;
