6b8fdf50d4861f46e0c9247040d3067690b62414
virender-codevian <virender.codevian@gmail.com>
2021-09-27 14:19:24 +0530

    new menu

--- src/components/navbar2/NavbarLinks.js
+++ src/components/navbar2/NavbarLinks.js
@@ -1,60 +1,60 @@
-import React from "react"
-// import { Link } from "gatsby"
-import {Link} from 'react-router-dom'
-import styled from "styled-components"
+import React from 'react';
+// import { Link } from 'gatsby';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const NavbarLinks = () => {
-  return (
-    <div>
-        <NavItem to="/">HOME</NavItem>
-      <NavItem to="/about">ABOUT</NavItem>
-      <NavItem to="/projects">WORK</NavItem>
-      <NavItem to="/blogs">BLOG</NavItem>
-      <NavItem to="/links">LINKS</NavItem>
-      <NavItem to="/contact">CONTACT</NavItem>
-      </div>
-  )
-}
+    return (
+        <>
+            <NavItem to="/">HOME</NavItem>
+            <NavItem to="/About">ABOUT</NavItem>
+            <NavItem to="/Projects">PROJECTS</NavItem>
+            <NavItem to="/Gallery">GALLERY</NavItem>
+            <NavItem to="/Blog">BLOG</NavItem>
+            <NavItem to="/Links">LINKS</NavItem>
+            <NavItem to="/Setup">SETUP</NavItem>
+            <NavItem to="/Contact">CONTACT</NavItem>
+        </>
+    );
+};
 
 const NavItem = styled(Link)`
-
-text-decoration: none;
-display: inline-block;
-white-space: nowrap;
-margin: 1vw 1vw;
-transition: all 200ms ease-in;
-position: relative;
-font-size: 0.7rem;
-font-weight: 600;
-color: black;
-font-family: Roboto, sans-serif;
-:after {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    width: 0%;
-    content: '';
-    color: transparent;
-    background: #fc6c85;
-    height: 1px;
-    transition: all 0.4s ease-in;
-}
-:hover {
-    ::after {
-        width: 100%;
+    text-decoration: none;
+    display: inline-block;
+    white-space: nowrap;
+    margin: 1vw 1vw;
+    transition: all 200ms ease-in;
+    position: relative;
+    font-size: 0.7rem;
+    font-weight: 600;
+    color: black;
+    font-family: Roboto, sans-serif;
+    :after {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        width: 0%;
+        content: '';
+        color: transparent;
+        background: #fc6c85;
+        height: 1px;
+        transition: all 0.4s ease-in;
     }
-}
-@media (max-width: 768px) {
-    padding: 8px 30px;
-    margin-left: 25px;
-    font-size: 30px;
-    z-index: 6;
-    align-self: baseline;
-    font-weight: 900;
-    font-family: Arial, sans-serif;
-}
+    :hover {
+        ::after {
+            width: 100%;
+        }
+    }
+    @media (max-width: 768px) {
+        padding: 8px 30px;
+        margin-left: 25px;
+        font-size: 30px;
+        z-index: 6;
+        align-self: baseline;
+        font-weight: 900;
+        font-family: Arial, sans-serif;
+    }
+`;
 
-`
-
-export default NavbarLinks
+export default NavbarLinks;
